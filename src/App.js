7532c5db5375070d3a321cf6b29667f64f8531a0
Shubham Srivastava <36308044+shubhfriendofall@users.noreply.github.com>
2024-08-31 12:37:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Shubham
+          Learn React with Shubham and Dhiraj
         </a>
       </header>
     </div>
