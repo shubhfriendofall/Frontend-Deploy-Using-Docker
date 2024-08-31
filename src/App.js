337de435adbbc6f53251cc9ef8b28c12f1cfe6c3
Shubham <shubhsrivastava15@gmail.com>
2024-08-31 13:28:22 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Shubham efficiently.
+          Learn React with Shubham smartlys.
         </a>
       </header>
     </div>
